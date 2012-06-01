9ff7f3784b3d264f9f1493a62cfe51acd63e6ac6
David Huber <david.huber@gmail.com>
2012-06-01 11:29:38 -0500

    Adding a container that sets up a server that's talking to a subscription service

---
+++ MassTransitTest/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MassTransitTest
+{
+    public static class Constants
+    {
+        public const string SubscriptionServiceUri = "";
+        public const string ReceiveFromUri = "";
+    }
+}
